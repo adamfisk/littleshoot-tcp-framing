a9d797f9924c1fef64f48b764ce93625371f204f
adamfisk <a@littleshoot.org>
2007-09-26 16:39:39 +0000

    Improved dependencies on IceMediaStream and added generic helper class to STUN visitor factories.
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/tcp-framing@1060 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/tcp/frame/TcpFrameEncoder.java
+++ src/main/java/org/lastbamboo/common/tcp/frame/TcpFrameEncoder.java
@@ -2,6 +2,8 @@ package org.lastbamboo.common.tcp.frame;
 
 import org.apache.mina.common.ByteBuffer;
 import org.lastbamboo.common.util.mina.MinaUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Encoder for framed TCP messages, as defined in RFC 4571. 
@@ -9,6 +11,8 @@ import org.lastbamboo.common.util.mina.MinaUtils;
 public class TcpFrameEncoder
     {
 
+    private final Logger m_log = LoggerFactory.getLogger(getClass());
+    
     /**
      * Encodes the TCP frame into a {@link ByteBuffer}.
      * 
@@ -22,6 +26,7 @@ public class TcpFrameEncoder
         MinaUtils.putUnsignedShort(buf, length);
         buf.put(frame.getData());
         buf.flip();
+        m_log.debug("Encoded TCP Frame as buffer: {}", buf);
         return buf;
         }
 
